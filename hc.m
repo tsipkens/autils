46aa7b28c20fe1a826e51f3d22eccc084d485209
tsipkens <tsipkens@gmail.com>
2022-02-03 23:46:55 -0500

    Update hc.m header

--- hc.m
+++ hc.m
@@ -1,6 +1,13 @@
 
 % HC  Use Hatch-Choate to compute new distribution moments. 
 %  
+%  D = hc(MU, SG, Q) uses Hatch-Choate to compute new distribution 
+%  moments having a GMD of MU and GSD of SG. Hatch-Choate applies the power
+%  of Q. 
+%  
+%  D = hc(MU, SG, Q, A) adds a pre-factor A and switched to the integrated
+%  variant. 
+%  
 %  AUTHOR: Timothy Sipkens, 2021-11-17
 
 function d = hc(mu, sg, q, a)
