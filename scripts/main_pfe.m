b7ff9d0d9cd95b0d26a0a2a67f7718077dbfbf29
tsipkens <tsipkens@gmail.com>
2022-03-23 13:09:21 -0400

    Refactor main_pfe

--- scripts/main_pfe.m
+++ scripts/main_pfe.m
@@ -2,6 +2,7 @@
 
 clear;
 close all;
+addpath cmap;
 
 
 
@@ -11,14 +12,14 @@ sg = 1.8;
 chi = 1.0;
 
 
-n = 220;
+n = 80;
 d = logspace(log10(10), log10(2e3), n)';
 
 prop = massmob.gen('salt');
 
 % PFE parameters. 
 npfe0 = 0.8;
-dmpps = 120;
+dmpps = 160;
 spfe = 3.5;
 
 x_up0 = 1e6 .* normpdf(log(d), log(dg), log(sg)) .* (log(d(2)) - log(d(1)));
@@ -32,33 +33,42 @@ mpfe0 = pfe.mpfe_hc(x_up0, x_down0, d, prop)  % noiseless MPFE
 
 % Generate noisy signals.
 % rng(0);
-ns = 10;
+ns = 25;
+ns2 = 3;
 [xs, ~, Gx] = uq.add_noise([x_up0; x_down0], 0.005, 1, 0.0001 .* max(x_up0), ns, randi(1e6));
 xs_up = xs(1:length(x_up0), :);
-x_up = mean(xs_up, 2); % x_up0; % mean(xs_up, 2);
+x_up = mean(xs_up(:, 1:ns2), 2); % x_up0; % mean(xs_up, 2);
 
 xs_down = xs(length(x_down0)+1:end, :);
-x_down = mean(xs_down, 2); % x_down0; % mean(xs_down, 2);
+x_down = mean(xs_down(:, 1:ns2), 2);  % only consider first three in mean (more realistic)
+
+% Standard deviations in counts.
+sx_up = sqrt(diag(Gx(1:length(d), 1:length(d))));
+sx_down = sqrt(diag(Gx(length(d)+1:end, length(d)+1:end)));
 
 figure(1);
-plot(d, xs_up, '.');
+plot(d, x_up0, 'k');
 hold on;
-plot(d, x_up, 'k');
-plot(d, x_down, 'k');
-plot(d, Pi .* max(x_up));
+plot(d, x_down0, 'k');
+plot(d, Pi .* max(x_up0), 'r');
+plot(d, x_up0 + 2 .* sx_up, 'k');
+plot(d, x_up0 - 2 .* sx_up, 'k');
+plot(d, x_down0 + 2 .* sx_down, 'k');
+plot(d, x_down0 - 2 .* sx_down, 'k');
 hold off;
 set(gca, 'XScale', 'log');
 
-
+% Number-based PFE.
 [npfe, s_npfe] = pfe.npfe(x_up, x_down, Gx ./ ns, []);
-pe = [s_npfe / npfe, npfe, npfe - npfe0]
+pe_npfe = [s_npfe / npfe, npfe, npfe - npfe0]
 
+% MPFE via numerical integration.
+[mpfe, s_mpfe] = pfe.mpfe_ni(x_up, x_down, d .* 1e-9, prop, Gx ./ ns, [], 0.01);
+pe_mpfe = [s_mpfe / mpfe, mpfe, mpfe - mpfe0]
 
-[mpfe, s_mpfe] = pfe.mpfe_ni(x_up, x_down, d .* 1e-9, prop, Gx ./ ns, [], 0.);
-pe = [s_mpfe / mpfe, mpfe, mpfe - mpfe0]
-
-[mpfe_hc, s_mpfe_hc] = pfe.mpfe_hc(x_up, x_down, d .* 1e-9, prop, Gx ./ ns, [], 0.);
-pe = [s_mpfe_hc / mpfe_hc, mpfe_hc, mpfe_hc - mpfe0]
+% MPFE via Hatch-Choate.
+[mpfe_hc, s_mpfe_hc] = pfe.mpfe_hc(x_up, x_down, d .* 1e-9, prop, Gx ./ ns, [], 0.01);
+pe_hc = [s_mpfe_hc / mpfe_hc, mpfe_hc, mpfe_hc - mpfe0]
 
 
 % Size-resolved filtration efficiencies.
@@ -73,17 +83,17 @@ for ii=1:size(xs_up, 2)
     spfes(:,ii) = pfe.spfe(xs_up(:,ii), xs_down(:,ii), Gx ./ ns, []);
 end
 
-
+% Plot size-resolved PFEs with uncertainty bounds.
 figure(2);
-
-addpath cmap;
 cmap_sweep(size(xs_up, 2), internet);
-
-scatter(d, spfes, 10, 'o', 'filled', 'MarkerFaceAlpha', 0.7);
+scatter(d, spfes, 8, 'o', 'filled', 'MarkerFaceAlpha', 1);
 hold on;
+scatter(d, spfe, 14, [0,0,0], 'o');
 plot(d, 1 - Pi, 'k');
 plot(d, 1 - Pi + 2 .* s_spfes0, 'k');
 plot(d, 1 - Pi - 2 .* s_spfes0, 'k');
+plot(d, 1 - Pi + 2 .* s_spfe0, 'k');
+plot(d, 1 - Pi - 2 .* s_spfe0, 'k');
 hold off;
 set(gca, 'XScale', 'log');
 ylim([0.5, 1.2]);
