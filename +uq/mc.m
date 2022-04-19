430e07db5ec8c7a0ba195da6f3ed0ad443e7b9ac
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-04-19 17:45:35 -0400

    Add uq.mc method

---
+++ +uq/mc.m
@@ -0,0 +1,19 @@
+
+% MC  Monte Carlo method.
+%  
+%  AUTHOR: Timothy Sipkens, 2022-02-10
+
+function [y, s, smpl, f_smpl] = mc(x, Gx, f, n)
+
+% If vector of variances supplied.
+if any(size(Gx) == 1)
+    Gx = diag(Gx);
+end
+
+smpl = mvnrnd(x, Gx, n)';
+
+f_smpl = f(smpl);
+y = mean(f_smpl, 2);
+s = std(f_smpl, [], 2);
+
+end
