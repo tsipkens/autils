d0619637819c60d1083b1a30b608c98c69676af4
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-04-19 17:45:22 -0400

    Add logn. fitting to get_geo

--- get_geo.m
+++ get_geo.m
@@ -1,6 +1,9 @@
 
 
-function [gmd, gsd] = get_geo(Ni, di)
+function [gmd, gsd, J] = get_geo(Ni, di, f_fit)
+
+if ~exist('f_fit', 'var'); f_fit = []; end
+if isempty(f_fit); f_fit = 0; end  % by default, use statistical definition
 
 gmd = exp(sum(Ni ./ nansum(Ni) .* ...
         log(di), 'omitnan'));
@@ -19,6 +22,24 @@ end
 % if any(~isreal([gmd, gsd]))
 %     disp('Imaginary GMD or GSD!');
 % end
+
+if f_fit  % instead, use lognormal fitting
+    di = di(~isnan(Ni));  % truncate NaN values
+    Ni = Ni(~isnan(Ni));
+
+    x0 = [max(Ni) .* sqrt(2*pi) .* log(gsd), ...
+        gmd, gsd];
+    
+    fun = @(x) Ni - ...
+        normpdf(log(di), log(x(2)), log(x(3))) .* ...
+        sqrt(2*pi) .* log(x(3)) .* x(1);
+    
+    opts.Display = 'none';
+    [x1, ~, ~, ~, ~, ~, J] = lsqnonlin(fun, x0, [], [], opts);
+    
+    gmd = x1(2);
+    gsd = x1(3);
+end
 
 end
 
