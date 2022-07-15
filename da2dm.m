36cf095bcda3359a85282a8092216b68751320bf
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-07-15 17:42:05 -0400

    Bug fix for da2dm
    When solving a vector at once, the calculation became unstable. Multiplied quantities by 1e9 for stability.

--- da2dm.m
+++ da2dm.m
@@ -1,22 +1,19 @@
 
-% DM2DA  Convert the aerodynamic diameter to a mobility diameter. 
+% DA2DM  Convert the aerodynamic diameter to a mobility diameter. 
 %  
-%  DM = da2dm(DA,PROP) converts the aerodynamic diameter, DA, to an 
+%  DM = da2dm(DA,PROP) converts the aerodynamic diameter, DA, to a 
 %  mobility diameter, DM, using the properties in PROP. 
 %  
-%  DM = dm2da(...,F_ITER) adds a flag indicating whether 
+%  DM = da2dm(...,F_ITER) adds a flag indicating whether 
 %  to use the simple (F_ITER = 0) or iterative (F_ITER = 1) 
 %  evaluation. The former is less accurate but faster. Default is
 %  F_SIMPLE = 1.
-%  
-%  [DM,DVE] = da2dm(...) adds an output for the volume equivalent 
-%  diameter of the particles. 
 %  
 %  ------------------------------------------------------------------------
 %  
 %  AUTHOR: Timothy Sipkens, 2022-01-27
 
-function [dm, dve0] = da2dm(da, prop, f_iter, varargin)
+function dm = da2dm(da, prop, f_iter, varargin)
 
 %-- Parse inputs --------------------------%
 if ~exist('f_iter', 'var'); f_iter = []; end
@@ -29,18 +26,20 @@ rho0 = 1e3;  % density of water
 % that is without iteration. 
 opts = optimset('Display', 'off');
 fun = @(dm) 1e9 .* ...
-    (dm .* sqrt(dm2rhoeff(dm, prop) ./ rho0) - da);
-dm = fsolve(fun, da, opts);
+    (dm .* 1e-9 .* sqrt(dm2rhoeff(dm .* 1e-9, prop) ./ rho0) - da);
+dm = fsolve(fun, da .* 1e9, opts) .* 1e-9;
 
 %-{
 % Alternate, iterative method that is more precise.
 if f_iter
     % Solve for mobility diameter. 
-    fun = @(dm) 1e9 .* (dm .* ...
-        sqrt(dm2rhoeff(dm, prop) ./ rho0 .* ...
-        Cc(dm, varargin{:}) ./ Cc(da, varargin{:})) - da);
-    dm = fsolve(fun, dm, opts);
+    fun = @(dm) 1e9 .* (dm .* 1e-9 .* ...
+        sqrt(dm2rhoeff(dm .* 1e-9, prop) ./ rho0 .* ...
+        Cc(dm .* 1e-9, varargin{:}) ./ Cc(da, varargin{:})) - da);
+    dm = fsolve(fun, dm .* 1e9, opts) .* 1e-9;
 end
 %}
+
+dm = real(dm);
 
 end
