cf1407f8cce80ce993d4175c85c74d083c51d5f9
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-07-25 12:52:29 -0400

    Increment script figure numbers

--- scripts/tut_fig2.m
+++ scripts/tut_fig2.m
@@ -1,43 +1,85 @@
 
-% A script demonstrating error models.
+% A script demonstrating error corruption of Gaussian signals.
+%  Roughly recreates Fig. 1.
 %  
-%  AUTHOR: Timothy Sipkens: 2022-04-19
+%  AUTHOR: Timothy Sipkens: 2022-06-23
 
 clear;
 close all;
-clc;
+addpath cmap;
 
-% Load mu and sig from real SMPS signal.
-load('data/smps1.mat');
 
-[~, tau, the, gam] = uq.covp(mu1, 'pgm', sig1);
+sd = 90;  % seed for random number generation
 
+% Distribution parameters.
+N = 4e3;
+dg = 75;
+sg = 1.5;
+
+% Discretization:
+% Roughly similar to SMPS spacing.
+n = 114;
+d = logspace(log10(15), log10(400), n)';
+dd = log(d(2)) - log(d(1));
+
+
+p0 = N .* normpdf(log(d), log(dg), log(sg)) .* dd;
+
+
+%== POISSON NOISE SUBPLOT ===============%
+[p1, ~, G1] = uq.add_noise(p0, 0, 1, 0, 3, sd, 1);  % add Poisson noise
+
+figure(1);
+subplot(1,2,1);
+plot(d, p0, 'k-');
+cmap_sweep(3, flipud(internet));
+hold on;
+plot(d, p1, '.', 'MarkerSize', 8);
+hold off;
+xlim([min(d), max(d)]);
+ylim([0, 200]);
+set(gca, 'XScale', 'log');
+
+%{
+% Generate plot on axis.
+idx1 = 57;  idx2 = 36;   % indices in the signal
+yvec = 0:0.01:200;  % for Gaussian representation
+ypvec = 0:1:200;  % for Poisson representation
 
 figure(2);
-
-%-- Real SMPS signal represented with PGM error model --------------------%
-plot(mu1, sig1 .^ 2, '.');
-set(gca, 'XScale', 'log', 'YScale', 'log');
-
-limy = ylim;
-limx = xlim;
-vec = logspace(log10(limx(1)), log10(limx(2)), 80);
+plot(normpdf(yvec, p0(idx1), sqrt(G1(idx1,idx1))), yvec, 'b-');
 hold on;
-plot(vec, gam .^ 2 .* ones(size(vec)));  % Gaussian
-plot(vec, the .* vec);  % Poisson
-plot(vec, (tau .* vec) .^ 2);  % multiplicative
-plot(vec, (tau .* vec) .^ 2 + the .* vec + gam .^ 2);  % PGM
+plot(normpdf(yvec, p0(idx2), sqrt(G1(idx2,idx2))), yvec, 'r-');
+ylim([0, 200]);
+
+% Add Poisson representation.
+ppdf = poisspdf(ypvec, p0(idx2));  % Compute Poisson distr.
+stairs(ppdf, ypvec+0.5, 'r');
 hold off;
+%}
 
-ylabel('Variance');
-xlabel('Mean / Expected value')
-ylim(limy);
 
-%-- Simulation Poisson signal --------------------------------------------%
-the2 = 10;
-s2 = uq.add_noise(mu1 ./ 5, 0, the2, 0, 200);
+%== PGM ERRORS SUBPLOT ==================%
+[p2, ~, G2] = uq.add_noise(p0, 0.25, 1, 0.2, 3, sd, 1);  % add PGM error
+p2 = p2(:, [2,3,1]);  % re-order (just for ascending color in plot)
+
+figure(1);
+subplot(1,2,2);
+plot(d, p0, 'k-');
+cmap_sweep(3, flipud(internet));
 hold on;
-plot(mu1 ./ 5, var(s2, [], 2), '.');
-plot(vec, the2 .* vec)
+plot(d, p2, '.', 'MarkerSize', 8);
 hold off;
+xlim([min(d), max(d)]);
+ylim([0, 200]);
+set(gca, 'XScale', 'log');
+
+%{
+figure(2);
+hold on;
+plot(normpdf(yvec, p0(idx1), sqrt(G2(idx1,idx1))), yvec, 'b-');
+plot(normpdf(yvec, p0(idx2), sqrt(G2(idx2,idx2))), yvec, 'r-');
+hold off;
+%}
+
 
--- scripts/tut_fig5.m
+++ scripts/tut_fig5.m
@@ -1,26 +1,26 @@
 
-% A script to generate Fig. 4 of Sipkens et al.
+% A script to generate Fig. 3 of Sipkens et al.
 
 clear;
 close all;
 addpath cmap;
 
-N = 2e4;
-rho_100 = 2160;
-dg = 75;
-sg = 1.5; % 1.05
+N = 1e4;
+rho_100 = 800;
+dg = 250;
+sg = 1.6;
 
 % Power law parameters.
 prop = massmob.init('zet', 3, 'rho100', rho_100)
 
 % Covariance information for power law.
-var_rho = (0.005 .* prop.rho100) ^ 2;
-var_zet = (0.005 .* prop.zet) ^ 2;
-cov_rho_zet = 0 .* sqrt(var_rho * var_zet);  % if correlated
+var_rho = (0.005 .* prop.rho100) ^ 2 * 0;
+var_zet = (0.005 .* prop.zet) ^ 2 * 0;
+cov_rho_zet = 0. * sqrt(var_rho * var_zet);  % if correlated
 G_rho_zet = [var_rho, cov_rho_zet; cov_rho_zet, var_zet];
 
 
-n0 = 2e4;  % overall number concentration
+n0 = 2e4;  % number of points in high diameter resolution distribution (for plotting)
 d0 = logspace(-2, 6, n0)';  % vector of diameters (high res.)
 dd0 = log(d0(2)) - log(d0(1));  % element width
 
@@ -44,97 +44,35 @@ di = d ./ 1e9;
 
 
 %== NUMERICAL INTEGRATION (NI) ===========================================%
+p_nim = N .* prop.m100 .* (d ./ 100) .^ prop.zet .* p;
+
 [M_ni, s_ni, ~, J_ni] = pm.pm_ni(p, di, prop, ...
     blkdiag(Gp, G_rho_zet));
 per_ni = s_ni / M_ni;
 
+% (Ni, di, prop, G)
 %== HATCH-CHOATE, SIMPLE (HCS) ===========================================%
-[M_hcs, s_hcs, ~, J_hcs] = pm.pm_hc(p, di, prop, ...
+[dg_hc, sg_hc] = get_geo(p, d);
+p_hcn = N .* normpdf(log(d0), log(dg_hc), log(sg_hc)) .* dd;
+p_hcm = N .* normpdf(log(d0), log(hc(dg_hc, sg_hc, prop.zet)), log(sg_hc)) .* dd;
+[M_hcs, s_hcs, J_hcs] = pm.pm_hc(p, di, prop, ...
     blkdiag(Gp, G_rho_zet));
 per_hcs = s_hcs / M_hcs;
 
 
-%== HATCH-CHOATE, LOGNORMAL (HCL) ========================================%
-[M_hcl, s_hcl] = pm.pm_hc_fit(p, di, prop, ...
-    blkdiag(Gp, G_rho_zet));
-per_hcl = s_hcl / M_hcl;
-
-
-%== HATCH-CHOATE, SIMPLE, INDEPENDENT N (HCS2) ===========================%
-[M_hcs_ind, s_hcs_ind] = pm.pm_hc_ind(p, di, prop, sum(p), ...
-    blkdiag(Gp, G_rho_zet, sum(sum(Gp))));
-per_hcs2 = s_hcs_ind / M_hcs_ind;
-
-
-%== MONTE CARLO (NI) =====================================================%
-x = [p1; prop.rho100; prop.zet];
-Gx = blkdiag(Gp, G_rho_zet);
-
-fk = @(x) x(end-1,:) .* (pi ./ 6 * (100e-9) ^ 3) .* (1 / 100) .^ x(end,:);
-f = @(x) fk(x) .* sum((di .* 1e9) .^ x(end,:) .* x(1:(end-2), :));
-[M_mcni, s_mcni, smpl, M_smpl] = uq.mc(x, Gx, f, 2e4, 6);
-per_mcni = s_mcni / M_mcni;
-
-
-%== MONTE CARLO (HCS) ====================================================
-f_N = @(x) sum(x(1:(end-2),:));
-f_dg = @(x) exp(1 ./ f_N(x) .* sum(x(1:(end-2),:) .* log(di)));
-f_sg = @(x) sqrt(1 ./ f_N(x) .* ...
-    sum(x(1:(end-2),:) .* (log(di) - log(f_dg(x))) .^ 2));
-f_H = @(x) fk(x) .* (f_dg(x) .* 1e9) .^ x(end,:) .* exp(x(end,:) .^ 2 ./ 2 .* f_sg(x) .^ 2);
-fh = @(x) f_N(x) .* f_H(x);
-[M_mch, s_mch, smplh, M_smplh] = uq.mc(x, Gx, fh, 2e4, 6);
-per_mch = s_mch / M_mch;
-
-
-
-% Output results.
-tbl = table([M_ni, M_hcs, M_hcs_ind, M_hcl, M_mcni, M_mch]' ./ M0(1), ...
-    [s_ni, s_hcs, s_hcs_ind, s_hcl, s_mcni, s_mch]', ...
-    100 .* [per_ni, per_hcs, per_hcs2, per_hcl, per_mcni, per_mch]', ...
-    'RowNames', {'NI', 'HCS', 'HCS(IND)', 'HCL', 'MC(NI)', 'MC(HC)'}, ...
-    'VariableNames', {'M/M0', 's', 'per'})
-
-
-% Compare NI and MC(MI).
-figure(4);
-[hy, hx] = histcounts(M_smpl ./ M0(1));
-[hyh] = histcounts(M_smplh ./ M0(1), hx);
-vec = linspace(0, max(hx), 1e3);
-
-stairs([hx(1), hx], [0, hy, 0], 'k');
+figure(3);
+dmax = 3e3;
+stairs([d; d(end); dmax] - dd/2, [p ./ sum(p .* dd); 0; 0], 'k');
 hold on;
-stairs([hx(1), hx], [0, hyh, 0], 'k');
-plot(vec, normpdf(vec, M_mcni ./ M0(1), s_ni ./ M0(1)) .* sqrt(2*pi) .* s_ni ./ M0(1) .* max(hy));
-plot(vec, normpdf(vec, M_mch ./ M0(1), s_hcs ./ M0(1)) .* sqrt(2*pi) .* s_ni ./ M0(1) .* max(hy));
-plot(vec, normpdf(vec, 1, s_hcl ./ M0(1)) .* sqrt(2*pi) .* s_ni ./ M0(1) .* max(hy));
-xline(1);
-% xline((M_ni + s_ni) ./ M0(1), 'r');  % lines indicated standard devs.
-% xline((M_hcs + s_hcs) ./ M0(1), 'g');
-% xline((M_hcl + s_hcl) ./ M0(1), 'b');
-% xline((M_ni - s_ni) ./ M0(1), 'r');
-% xline((M_hcs - s_hcs) ./ M0(1), 'g');
-% xline((M_hcl - s_hcl) ./ M0(1), 'b');
+stairs([d; 0; 0] - dd/2, [p_nim ./ sum(p .* dd) ./ M0(1); 0; 0], 'k');
+plot(d0, p_hcn ./ sum(p_hcn .* dd0), 'Color', [0.1,0.2,0.8]);
+plot(d0, p_hcm ./ sum(p_hcm .* dd0), 'Color', [0.1,0.2,0.8]);
+plot(d0, p0 ./ sum(p0 .* dd0), 'r');
+% stairs();
+xline(max(d), 'k--');
 hold off;
 
-xlim([0.7, 1.3]);
-
-
-% Plot of scaled Jacobian for Ntot, NI, and HCS.
-figure(1);
-plot(d, ones(size(p1)) ./ N);
-hold on;
-plot(d, J_ni(1:end-2) ./ M0(1));
-plot(d, J_hcs(1:end-2) ./ M0(2));
-hold off;
-set(gca, 'YScale', 'log', 'XScale', 'log');
-xlim([min(d), max(d)]);
-ylim([1e-5, 1e-2]);
-
-
-
-
-
-
+set(gca, 'XScale', 'log');
+xlim([min(d), dmax]);
 
 
