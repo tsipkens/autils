1abe11ff781f623f83559f0f54698d8bd1d34e12
Timothy Sipkens <tsipkens@uwaterloo.ca>
2022-07-27 11:36:27 -0400

    Refactor tut_fig1
    + Update .gitignore to ignore archive

--- scripts/tut_fig1.m
+++ scripts/tut_fig1.m
@@ -1,6 +1,6 @@
 
-% A script demonstrating error corruption of Gaussian signals.
-%  Roughly recreates Fig. 2.
+% A script demonstrating error corruption of signals for lognormal distribution.
+%  Roughly recreates Fig. 1.
 %  
 %  AUTHOR: Timothy Sipkens: 2022-06-23
 
@@ -11,6 +11,8 @@ addpath cmap;
 
 sd = 90;  % seed for random number generation
 
+
+%== DEFINE BASE SIGNAL/DISTRIBUTION ======================================%
 % Distribution parameters.
 N = 4e3;
 dg = 75;
@@ -22,11 +24,11 @@ n = 114;
 d = logspace(log10(15), log10(400), n)';
 dd = log(d(2)) - log(d(1));
 
-
 p0 = N .* normpdf(log(d), log(dg), log(sg)) .* dd;
+%=========================================================================%
 
 
-%== POISSON NOISE SUBPLOT ===============%
+%== POISSON NOISE SUBPLOT ================================================%
 [p1, ~, G1] = uq.add_noise(p0, 0, 1, 0, 3, sd, 1);  % add Poisson noise
 
 figure(1);
@@ -41,7 +43,7 @@ ylim([0, 200]);
 set(gca, 'XScale', 'log');
 
 %{
-% Generate plot on axis.
+% Generate plot on right axis.
 idx1 = 57;  idx2 = 36;   % indices in the signal
 yvec = 0:0.01:200;  % for Gaussian representation
 ypvec = 0:1:200;  % for Poisson representation
@@ -59,7 +61,7 @@ hold off;
 %}
 
 
-%== PGM ERRORS SUBPLOT ==================%
+%== PGM ERRORS SUBPLOT ===================================================%
 [p2, ~, G2] = uq.add_noise(p0, 0.25, 1, 0.2, 3, sd, 1);  % add PGM error
 p2 = p2(:, [2,3,1]);  % re-order (just for ascending color in plot)
 
@@ -73,13 +75,5 @@ hold off;
 xlim([min(d), max(d)]);
 ylim([0, 200]);
 set(gca, 'XScale', 'log');
-
-%{
-figure(2);
-hold on;
-plot(normpdf(yvec, p0(idx1), sqrt(G2(idx1,idx1))), yvec, 'b-');
-plot(normpdf(yvec, p0(idx2), sqrt(G2(idx2,idx2))), yvec, 'r-');
-hold off;
-%}
 
 
